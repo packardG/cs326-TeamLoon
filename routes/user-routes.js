41b3553bf617c0dd4c976e46cd6f88e708b8729a
Patrick Hurley <pjhurley@umass.edu>
2015-11-21 13:54:36 -0500

    myfirstmessage

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -17,7 +17,7 @@ router.get('/login', (req, res) => {
 
   // Redirect to main if cookie and user is online:
   if (user && online[user.name]) {
-    res.redirect('/user/main');
+    res.redirect('');
   }
   else {
     // Grab any messages being sent to us from redirect:
