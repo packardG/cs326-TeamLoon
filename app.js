fd3e0d96eff1d56122e7b6a2648ea8cde2f01c03
Packard Gilson <packardgilson@gmail.com>
2015-11-23 18:00:08 -0500

    merge

--- app.js
+++ app.js
@@ -157,22 +157,6 @@ app.get('/aboutsofaking', (req, res) => {
 });
 
 
-function room(name, desc, lat, long) {
-  return {
-    name: name,
-    desc: desc,
-    lat: lat,
-    long: long
-  };
-}
-
-var rooms = [
-  room('amherst', 'bleh', 0, 0),
-  room('sunderland', 'neverland', 0, 0),
-  room('')
-];
-
-
 app.get('/roomView', (req, res) => {
 
     res.render('wireframe', {
