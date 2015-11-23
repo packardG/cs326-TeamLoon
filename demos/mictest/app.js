b5e9ec13480e8688ecca63f3e6e66cb844e8d4e7
Noobulater <AmanBentley@gmail.com>
2015-11-23 16:33:35 -0500

    final changes

--- demos/mictest/app.js
+++ demos/mictest/app.js
@@ -26,7 +26,17 @@ app.listen(app.get('port'), () => {
 
 
 // PEERJS stuff
-srv = app.listen(27000)
-app.use('/peerjs', require('peer').ExpressPeerServer(srv, {
+server = app.listen(27000)
+app.use('/peerjs', require('peer').ExpressPeerServer(server, {
 	debug: true
 }))
+
+var connected = [];
+server.on('connection', function (id) {
+  var idx = connected.indexOf(id); // only add id if it's not in the list yet
+  if (idx === -1) {connected.push(id);}
+});
+server.on('disconnect', function (id) {
+  var idx = connected.indexOf(id); // only attempt to remove id if it's in the list
+  if (idx !== -1) {connected.splice(idx, 1);}
+});
