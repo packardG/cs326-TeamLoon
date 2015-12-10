13a35ef13c6ab1163387bf18718bbec9f9d86452
Timothy Ng <tdng@umass.edu>
2015-12-10 00:29:57 -0500

    Put routing information in routes/routes.js

--- app.js
+++ app.js
@@ -8,18 +8,12 @@ var flash = require('connect-flash');
 var morgan = require('morgan');
 
 
-//local libraries
-var db = require('./lib/db');
-var team = require('./lib/team.js');
 
-var maintanace = false;
 
 // initialize our app
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-app.set('port', process.env.PORT || 3000);
 
 var view = handlebars.create({ defaultLayout: 'main' });
 app.engine('handlebars', view.engine);
@@ -43,173 +37,11 @@ app.use(session({
 
 //
 // handle routes
+var routes = require("./routes/routes.js")(app);
 
 function loggedIn(sessionUser) {
   return sessionUser && db.isOnline(sessionUser);
 }
-
-app.post('/auth',(req,res) => {
-  // Grab the session if the user is logged in.
-  var sessionUser = req.session.user;
-  // Redirect to main if cookie and user is online:
-  if (loggedIn(sessionUser)) {
-    res.redirect('/about');
-  }
-  else {
-    // Pull the values from the form:
-    var name = req.body.name;
-    var pass = req.body.pass;
-
-    if (!name || !pass) {
-      req.flash('login', 'Did not provide the proper credentials.');
-      res.redirect('/login');
-    }
-    else {
-      // this isn't a big part of our app, so user object is really simple
-      var user = {username : name, password : pass};
-      // callbacks
-      function success() {
-        req.session.user = user;
-        res.redirect('/');
-      }
-      function failure() {
-        req.flash('login', 'Account Doesn\'t Exist');
-        res.redirect('/login');
-      }
-      db.validateUser(user, success, failure);
-    }
-  }
-  //
-});
-
-app.get('/login',(req,res) => {
-  res.render('login', {
-    title : "Oops, sorry! A janitor is cleaning up some nasty code",
-    message : "We'll be back in a jiff. If you are an admin please log in here!",
-   //  layout : "none",
-  });
-});
-
-app.get('/logout', function(req, res) {
-  // Grab the user session if logged in.
-  var sessionUser = req.session.user;
-
-  if (sessionUser && !db.isOnline(sessionUser)) {
-    delete req.session.user;
-  }
-  // Otherwise, we delete both.
-  else if (loggedIn(sessionUser)) {
-    db.logout(sessionUser);
-    delete req.session.user;
-  }
-
-  // Redirect regardless.
-  res.redirect('/');
-});
-
-
-app.get('/', (req, res) => {
-    var sessionUser = req.session.user;
-    if (maintanace && !loggedIn(sessionUser)) {
-      res.redirect('/login');
-      return;
-    }
-    res.render('splash');
-});
-
-app.get('/team', (req, res) => {
-  var sessionUser = req.session.user;
-  if (maintanace && !loggedIn(sessionUser)) {
-    res.redirect('/login');
-    return;
-  }
-
-  var result;
-  var queryVal = req.query.user;
-
-  //Check to see if query was valid
-  if(!queryVal){
-    result = team.all();
-  }
-  else{
-    result = team.one(queryVal);
-  }
-
-  if (!result.success) {
-    notFound404(req, res);
-  } else {
-    res.render('team', {
-      members: result.data,
-    });
-  }
-});
-
-app.get('/about', (req, res) => {
-  var sessionUser = req.session.user;
-  if (maintanace && !loggedIn(sessionUser)) {
-    res.redirect('/login');
-    return;
-  }
-
-  var result = team.all();
-  if (!result.success) {
-    notFound404(req, res);
-  } else {
-    res.render('about');
-  }
-});
-
-app.get('/testChatRoom', (req, res) => {
-  var sessionUser = req.session.user;
-  if (maintanace && !loggedIn(sessionUser)) {
-    res.redirect('/login');
-    return;
-  }
-
-  var result = team.all();
-  if (!result.success) {
-    notFound404(req, res);
-  } else {
-    res.render('testChatRoom');
-  }
-});
-
-//Used to create temporary fake chatrooms for our chatroom-selection view.
-function room(name, desc, lat, long) {
-  return {
-    name: name,
-    desc: desc,
-    lat: lat,
-    long: long
-  };
-}
-var rooms = [
-  room('amherst', 'bleh', 0, 0),
-  room('sunderland', 'neverland', 0, 0),
-  room('hadley', 'mall place', 0, 0)
-];
-
-app.get('/roomView', (req, res) => {
-  var sessionUser = req.session.user;
-  if (maintanace && !loggedIn(sessionUser)) {
-    res.redirect('/login');
-    return;
-  }
-  res.render('wireframe', {
-    layout : "chatroom",
-  });
-});
-
-app.get('/roomSelection', (req, res) => {
-  var sessionUser = req.session.user;
-  if (maintanace && !loggedIn(sessionUser)) {
-    res.redirect('/login');
-    return;
-  }
-  res.render('chatroom-selection', {
-    rooms: rooms
-  });
-});
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
@@ -218,22 +50,7 @@ io.on('connection', function(socket){
   });
 });
 
-//Errors
-function notFound404(req, res) {
-  res.status(404);
-  res.render('404', {layout : 'none'});
-}
-
-function internalServerError500(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-  res.render('500', {layout : 'none'});
-}
-
-app.use(notFound404);
-app.use(internalServerError500);
-
-http.listen(3000, () => {
+http.listen(3000, '0.0.0.0', () => {
   console.log('Express started on http://localhost:' +
       3000 + '; press Ctrl-C to terminate');
 });
---
+++ routes/routes.js
@@ -0,0 +1,188 @@
+module.exports = function (app) {
+    var maintanace = false;
+
+    //local libraries
+    var db = require('../lib/db');
+    var team = require('../lib/team.js');
+
+    app.post('/auth',(req,res) => {
+        // Grab the session if the user is logged in.
+        var sessionUser = req.session.user;
+        // Redirect to main if cookie and user is online:
+        if (loggedIn(sessionUser)) {
+            res.redirect('/about');
+        }
+        else {
+            // Pull the values from the form:
+            var name = req.body.name;
+            var pass = req.body.pass;
+
+            if (!name || !pass) {
+                req.flash('login', 'Did not provide the proper credentials.');
+                res.redirect('/login');
+            }
+            else {
+                // this isn't a big part of our app, so user object is really simple
+                var user = {username : name, password : pass};
+                // callbacks
+                function success() {
+                    req.session.user = user;
+                    res.redirect('/');
+                }
+                function failure() {
+                    req.flash('login', 'Account Doesn\'t Exist');
+                    res.redirect('/login');
+                }
+                db.validateUser(user, success, failure);
+            }
+        }
+        //
+    });
+
+    app.get('/login',(req,res) => {
+        res.render('login', {
+            title : "Oops, sorry! A janitor is cleaning up some nasty code",
+            message : "We'll be back in a jiff. If you are an admin please log in here!",
+            //  layout : "none",
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        // Grab the user session if logged in.
+        var sessionUser = req.session.user;
+
+        if (sessionUser && !db.isOnline(sessionUser)) {
+            delete req.session.user;
+        }
+        // Otherwise, we delete both.
+        else if (loggedIn(sessionUser)) {
+            db.logout(sessionUser);
+            delete req.session.user;
+        }
+
+        // Redirect regardless.
+        res.redirect('/');
+    });
+
+
+    app.get('/', (req, res) => {
+        var sessionUser = req.session.user;
+        if (maintanace && !loggedIn(sessionUser)) {
+            res.redirect('/login');
+            return;
+        }
+        res.render('splash');
+    });
+
+    app.get('/team', (req, res) => {
+        var sessionUser = req.session.user;
+        if (maintanace && !loggedIn(sessionUser)) {
+            res.redirect('/login');
+            return;
+        }
+
+        var result;
+        var queryVal = req.query.user;
+
+        //Check to see if query was valid
+        if(!queryVal){
+            result = team.all();
+        }
+        else{
+            result = team.one(queryVal);
+        }
+
+        if (!result.success) {
+            notFound404(req, res);
+        } else {
+            res.render('team', {
+                members: result.data,
+            });
+        }
+    });
+
+    app.get('/about', (req, res) => {
+        var sessionUser = req.session.user;
+        if (maintanace && !loggedIn(sessionUser)) {
+            res.redirect('/login');
+            return;
+        }
+
+        var result = team.all();
+        if (!result.success) {
+            notFound404(req, res);
+        } else {
+            res.render('about');
+        }
+    });
+
+    app.get('/testChatRoom', (req, res) => {
+        var sessionUser = req.session.user;
+        if (maintanace && !loggedIn(sessionUser)) {
+            res.redirect('/login');
+            return;
+        }
+
+        var result = team.all();
+        if (!result.success) {
+            notFound404(req, res);
+        } else {
+            res.render('testChatRoom');
+        }
+    });
+
+//Used to create temporary fake chatrooms for our chatroom-selection view.
+    function room(name, desc, lat, long) {
+        return {
+            name: name,
+            desc: desc,
+            lat: lat,
+            long: long
+        };
+    }
+    var rooms = [
+        room('amherst', 'bleh', 0, 0),
+        room('sunderland', 'neverland', 0, 0),
+        room('hadley', 'mall place', 0, 0)
+    ];
+
+    app.get('/roomView', (req, res) => {
+        var sessionUser = req.session.user;
+        if (maintanace && !loggedIn(sessionUser)) {
+            res.redirect('/login');
+            return;
+        }
+        res.render('wireframe', {
+            layout : "chatroom",
+        });
+    });
+
+    app.get('/roomSelection', (req, res) => {
+        var sessionUser = req.session.user;
+        if (maintanace && !loggedIn(sessionUser)) {
+            res.redirect('/login');
+            return;
+        }
+        res.render('chatroom-selection', {
+            rooms: rooms
+        });
+    });
+
+//Errors
+    function notFound404(req, res) {
+        res.status(404);
+        res.render('404', {layout : 'none'});
+    }
+
+    function internalServerError500(err, req, res, next) {
+        console.error(err.stack);
+        res.status(500);
+        res.render('500', {layout : 'none'});
+    }
+
+    app.use(notFound404);
+    app.use(internalServerError500);
+
+};/**
+ * Created by Hawmalt on 12/10/2015.
+ */
